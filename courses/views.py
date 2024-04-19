92abc7426d8096fd20fb825bb13e2a5191b03ad1
danila <spook9400@mail.ru>
2024-04-19 11:46:42 +0300

    add homework 25.1

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsOwner, IsModerator, IsNotModerator
 from .models import Lesson
